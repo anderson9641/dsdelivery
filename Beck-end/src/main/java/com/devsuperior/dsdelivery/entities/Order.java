5fbd8615663c54078699a3fdc897b3d0e8e40b22
Anderson Alves <anderson96627210@gmail.com>
2021-01-04 18:50:35 -0300

    get /orders

--- Beck-end/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
+++ Beck-end/src/main/java/com/devsuperior/dsdelivery/entities/Order.java
@@ -35,7 +35,7 @@ public class Order implements Serializable{
 	@JoinTable(name="tb_order_product", 
 		joinColumns = @JoinColumn(name="order_id"),
 		inverseJoinColumns = @JoinColumn(name="product_id"))
-	private Set<Product> produtcs = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 	
 	public Order() {
 		
@@ -100,7 +100,7 @@ public class Order implements Serializable{
 	}
 
 	public Set<Product> getProdutcs() {
-		return produtcs;
+		return products;
 	}
 
 	@Override
