2f38adc8706cf729ec4bb52d79aa5e5ee7b9219d
Anderson Alves <anderson96627210@gmail.com>
2021-01-06 21:09:17 -0300

    Add routers and stepsheader

---
+++ front-web/src/Order/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+import StepsHeader from './StepsHeader';
+
+function Orders(){
+
+    return(
+        <div>
+            <StepsHeader/>
+        </div>
+    )
+}
+
+export default Orders;
