004e088a7cb53937b92cb62334f308c1a34f73fa
Anderson Alves <anderson96627210@gmail.com>
2021-01-09 20:34:22 -0300

    correção de erro

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,5 +1,5 @@
 import './style.css';
-import {ReactComponent as Logo} from './logo.svg';
+import {ReactComponent as Logo} from './Logo.svg';
 import { Link } from 'react-router-dom';
 
 function Navbar(){
